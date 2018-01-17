daf7c19eea26b35c2cc1044570003fa13b55a00b
bhawna <bhawna@knoldus.in>
2018-01-17 15:17:45 +0530

    adding gitignore

--- src/main/scala/edu/knoldus/Person.scala
+++ src/main/scala/edu/knoldus/Person.scala
@@ -11,7 +11,7 @@ case class Gamer() extends Person
 
 case class Trainer() extends Person
 
-case class Blogger(blogList: Map[String, Int], noofblogs: Int) extends Person
+case class Blogger(bloglist: Map[String, Int]) extends Person
 
 
 object Person extends App {
@@ -28,11 +28,13 @@ object Person extends App {
                       diceHandling(a, 1)
       case Trainer() => log.info("\n")
                         val c = 50
-                        s"Trainer Response: Class strength is ${Random.nextInt(c)}"
+                        s"Trainer Response: Class strength is ${Random.nextInt(c)} \n"
+      case Blogger(bloglist) => updationOfBlogging(bloglist)
     }
   }
 
   private def diceHandling(number: Int, count: Int): String = {
+
 
     number match {
       case 1 | 6 if (count < 3) => val b = 6
@@ -42,12 +44,25 @@ object Person extends App {
                                    diceHandling(a, count + 1)
       case 1 | 6 if (count == 3) => "Gamer Response : you are the winner"
       case 2 | 3 | 4 | 5 => "Gamer Response : you are the looser"
+      case 0 => " this is not possible"
 
     }
   }
 
+  private def updationOfBlogging(bloglist: Map[String, Int]): String = {
+
+    val c = bloglist.keys
+    val d = c.toList(Random.nextInt(c.size))
+    val e = bloglist(d)
+    val newbloglist = bloglist + (d -> (e + 1))
+    val favtopic  = newbloglist.keysIterator.max
+    s" Blogger Response: maximum blogging is done in $favtopic"
+  }
+  val bloglist = Map("c++" -> 1, "java" -> 5, "C#" ->4 )
   log.info(action(Gamer()))
   log.info(action(Trainer()))
+  log.info(action(Blogger(bloglist)))
 }
+
 
 
